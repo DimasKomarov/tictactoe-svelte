772dc33eafc802a5a1243ff38f477b4619954095
DimasKomarov <102760182+DimasKomarov@users.noreply.github.com>
2022-05-01 20:45:17 +0300

    main

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,102 @@
 <script>
-	import Space from "./Space.svelte"
-</script>
+	
+	// Имортируем классы
+	import Space from "./Space.svelte";
+	import gameStore from "./game-store.js";
+	import { nextMove } from "./requests.js";
+	
+	// Добавляем переменные 
+	let board = ["", "", "", "", "", "", "", "", "", "", ""];
+	let nextPlayer = "";
+	let winner;
+	let numberOfPeeps = 0;
+	let errorMessage;
+	
+	// Игровой прогресс
+	gameStore.subscribe(data => {
+	  if (!data) {
+		return;
+	  }
+	  winner = data.winner;
+	  nextPlayer = data.nextPlayer;
+	  board = data.board;
+	  numberOfPeeps = data.numberOfPeeps;
+	});
+	
+	// Подключение к игре
+	async function takeSpace(space) {
+	  if (winner || !gameStore.isConnected) {
+		return;
+	  }
+	  errorMessage = await nextMove(space);
+	}
+  </script>
+  
 
-<Space />
+  <!-- Стили -->
+  <style>
+	main {
+	  width: 475px;
+	  margin: 0 auto;
+	  height: 1000px;
+	}
+	.row {
+	  display: flex;
+	}
+	button {
+	  width: 100%;
+	  margin-top: 20px;
+	  border-radius: 0;
+	  background-color: lightblue;
+	  font-size: 30px;
+	  cursor: pointer;
+	}
+	button:hover {
+	  outline: none;
+	}
+	.errorMessage {
+	  color: red;
+	  font-size: 20px;
+	}
+  </style>
+  
+
+  <!-- Мейн с таблицей (основной частью) -->
+  <main>
+	<h1>Tic Tac Toe</h1>
+	<h2>Number of people playing: {numberOfPeeps}</h2>
+	{#if winner == 'TIE'}
+	  <h2>Tie Game!!!</h2>
+	{:else if winner}
+	  <h2>Player {winner} won!!!</h2>
+	{:else}
+	  <h2>Player: {nextPlayer}</h2>
+	{/if}
+	
+	<!-- Таблица с ячейками -->
+	<div class="row">
+	  <Space {winner} space={board[0]} on:click={() => takeSpace(0)} />
+	  <Space {winner} space={board[1]} on:click={() => takeSpace(1)} />
+	  <Space {winner} space={board[2]} on:click={() => takeSpace(2)} />
+	</div>
+	<div class="row">
+	  <Space {winner} space={board[3]} on:click={() => takeSpace(3)} />
+	  <Space {winner} space={board[4]} on:click={() => takeSpace(4)} />
+	  <Space {winner} space={board[5]} on:click={() => takeSpace(5)} />
+	</div>
+	<div class="row">
+	  <Space {winner} space={board[6]} on:click={() => takeSpace(6)} />
+	  <Space {winner} space={board[7]} on:click={() => takeSpace(7)} />
+	  <Space {winner} space={board[8]} on:click={() => takeSpace(8)} />
+	</div>
+	
+	<!-- Условия для победителя -->
+	{#if winner}
+	  <button>New Game</button>
+	{/if}
+	
+	<!-- Условия в случае ошибки -->
+	{#if errorMessage}
+	  <p class="errorMessage">{errorMessage}</p>
+	{/if}
+  </main>
--- src/Space.svelte
+++ src/Space.svelte
@@ -1,5 +1,23 @@
+<!-- Экспортировали переменную -->
 <script>
     export let space = "";
 </script>
 
-<div class="player">{space}</div>
+<!-- Стили таблицы и ячеек -->
+<style>
+    .space {
+        border: solid 1px gray;
+        height: 125px;
+        width: 125px;
+        flex: 1;
+        font-size: 75px;
+        text-align: center;
+        line-height: 125px;
+        cursor: pointer;
+    }
+</style>
+
+
+
+<!-- Блок див с ником игрока -->
+<div class="space">{space}</div>
